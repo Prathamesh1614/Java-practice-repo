ad5bc57e04346b3f4681c76a04ec109804d159fe
Prathamesh Shinde <shindeprathamesh1608@gmail.com>
2026-01-10 11:45:06 +0530

    satrt of java

---
+++ add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class add 
+{
+    public static void main(String[] args)
+     {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number:");
+        a=sc.nextInt();
+        System.out.println("Enter second Number:");
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("Addition is="+c);
+        
+    }
+    
+}
