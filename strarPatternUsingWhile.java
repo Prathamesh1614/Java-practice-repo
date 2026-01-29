07fca71d94abd66ebe1816c2cf0886c9b1d48c51
Prathamesh Shinde <shindeprathamesh1608@gmail.com>
2026-01-29 12:28:32 +0530

    printing star pattern using while loop

---
+++ strarPatternUsingWhile.java
@@ -0,0 +1,17 @@
+public class strarPatternUsingWhile {
+    public static void main(String args[]) {
+        int i = 4;
+        while (i >= 1) {
+            int j = 1;
+
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i--;
+        }
+
+    }
+
+}
